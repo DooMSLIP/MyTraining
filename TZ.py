984b188d2c331f05489f6a60d8a019df9ccbf77d
DooMSLIP <doomslip@gmail.com>
2023-04-22 19:22:10 +0700

    Create TZ.py

---
+++ TZ.py
@@ -0,0 +1,14 @@
+for a in 0,1:
+    for b in 0,1:
+        for c in 0,1:
+            for d in 0,1:
+                x = a!=b
+                y = not x
+                w = not b
+                z =y or w
+                k = z and c
+                l = c or d
+                m = not l
+                n = k!=m
+                if n == True:
+                    print (a,b,c,d)
